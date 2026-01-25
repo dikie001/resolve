2aff7caf9cd9f99133e09a96f4bcbc0a847e65a4
dikie001 <omondidickens255@gmail.com>
2026-01-25 21:19:46 +0300

    initialize resolve app feat: implement sidebar component with context and mobile support
    - Added Sidebar component with collapsible functionality and keyboard shortcuts.
    - Created SidebarProvider for managing sidebar state.
    - Implemented SidebarTrigger, SidebarRail, and SidebarInset for better UI interaction.
    - Introduced SidebarMenu and related components for organizing menu items.
    - Added Skeleton component for loading states.
    - Created Tabs component for tabbed navigation.
    - Implemented Tooltip component for enhanced user guidance.
    - Established global styles and utility functions for class management.
    - Set up routing with ResolveMainPage as the initial route.
    - Configured TypeScript and Vite for project structure and build process.

---
+++ src/pages/ResolveMainPage.tsx
@@ -0,0 +1,8 @@
+
+const ResolveMainPage = () => {
+  return (
+    <div>ResolveMainPage</div>
+  )
+}
+
+export default ResolveMainPage
---
+++ src/routes/index.tsx
@@ -0,0 +1,16 @@
+import ResolveMainPage from "@/pages/ResolveMainPage";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+export const AppRoutes = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<ResolveMainPage />} />
+        </Routes>       
+      </Router>
+    </div>
+  );
+};
+
+export default AppRoutes;
